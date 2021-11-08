d47448d11603e3426a5cc5ffb63d0ea68f6db28c
Milton Chandradas <milton.chandradas@sap.com>
2021-11-08 22:06:03 +0000

    initial commit...

--- webapp/utils/excelUtils.js
+++ webapp/utils/excelUtils.js
@@ -10,10 +10,45 @@ sap.ui.define([], function () {
 			table.style.cssText = "font-family: sans-serif; font-size: 0.9em; border: 1px solid rgb(174, 215, 255);";
 
 			return table;
+        },
+        
+        _addColumnHeaders: function (table, columnHeaders) {
+
+			let tr = document.createElement("tr");
+			tr.style.color = "#fff";
+			tr.style.textAlign = "left";
+			tr.style.fontWeight = "bold";
+
+			// Add the column headers
+			columnHeaders.forEach((header) => {
+				let th = document.createElement("th");
+				th.innerHTML = header;
+				th.style.backgroundColor = "rgb(40, 110, 180)";
+				th.style.padding = "12px 15px";
+				th.style.height = "40px";
+				th.style.border = "1px solid rgb(174, 215, 255)";
+				tr.appendChild(th);
+			});
+
+			table.appendChild(tr);
+
+			return tr;
 		},
 
         createDOMTable: function (controller) {
             let table = this._createTable("domTable");
+            let columnHeaders = [
+                "Product Name",
+                "Supplier ID",
+                "Category ID",
+                "Quantity Per Unit",
+                "Unit Price",
+                "Units In Stock",
+                "Discontinued"
+
+            ];
+            let tr = this._addColumnHeaders(table, columnHeaders);
+
             return table;
         },
 
